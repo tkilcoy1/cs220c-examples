6edb6836fc8b29166a63e850d00ab3c817c4c854
tkilcoy1 <tkilcoy1@binghamton.edu>
2025-02-09 15:11:53 -0500

    update

--- redirection/printMultiples.c
+++ redirection/printMultiples.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main(int argc, char ** argv){
+  if (argc < 3) {
+    fprintf(stderr, "Usage: ./printMultiples a max\n");
+    return 1;
+  }
   int a = atoi(argv[1]);
   int max = atoi(argv[2]);
   for (int i = 1; i*a <= max; i++){
--- strings/strTest.c
+++ strings/strTest.c
@@ -3,10 +3,16 @@
 #include <string.h>
 
 int main(){
-  char str1[10];
-  char * str2;
+  char * str1 = malloc(sizeof(char)*20);
+  strcpy(str1, "hello");
+  char * str2 = ", World!";
 
-  // TODO: concatenate str1 and str2 to "Hello, World!" 
+  str1[0] = 'H';
+  strcat(str1, str2);
+
+  printf("%s\n", str1);
+
+  // concatenate str1 and str2 to "Hello, World!" 
 
   return 0;
 }
